3619f8bbaab934ec9f68d54869d2f41b47b111ac
OnsChebi <chebbions7@gmail.com>
2026-06-08 11:03:00 +0100

    reverse function

---
+++ easy/easyProblems.py
@@ -0,0 +1,7 @@
+###############reverse fucntion
+def reverse (word):
+    reversed_word=""
+    for i in range(len(word)-1, -1, -1):
+        reversed_word+=word[i]
+    return reversed_word
+print(reverse('noussa'))
